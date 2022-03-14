11f1d4e052e0f1a70fe803efb72da76874dc2198
Elisabeth Meyer <eameyer@sh02-ln01.stanford.edu>
2022-03-14 15:01:14 -0700

    Changed count.R script to take in bed file of annotations like gene names to use as the bins

--- bin/count.R
+++ bin/count.R
@@ -11,7 +11,17 @@ basename <- args[2]
 libType <- args[3]
 binSize <- as.numeric(args[4])
 
-## Function to get bin from position, strand, and chromsome
+
+
+
+## To put into arguments eventually...
+geneVersion <- T
+#gene_bed_path <- "/scratch/groups/horence/Elisabeth/intron_zscore/Arabidopsis/ref_files/Arabidopsis_3UTR_forReadZS.tsv"
+gene_bed_path <- "/scratch/groups/horence/Elisabeth/intron_zscore/Arabidopsis/ref_files/Arabidopsis_genes_forReadZS.tsv"
+
+
+
+## Function to get bin (genomic window) from position, strand, and chromsome
 get_bin <- function(pos, binSize, chr, strand)
 {
   pos <- as.numeric(pos)
@@ -19,6 +29,27 @@ get_bin <- function(pos, binSize, chr, strand)
   bin <- paste(chr, bin_num, strand, sep="_")
   return(bin)
 }
+
+
+## Function adapted from Rob Bierman to create bins from a bed file of genes (or UTRs, or other annotations)
+get_bin_genes <- function(pos_list, binSize, chrom_list, strand, libraryType, bed_path)
+{
+  gene_locs <- fread(bed_path)
+  bins <- vector("list", length(pos_list))
+  for(i in 1:length(pos_list)){
+    pos <- as.numeric(pos_list[i])
+    chrom <- chrom_list[i]
+    gene_name <- gene_locs[(gene_locs[, (chr == chrom) & (start <= pos) & (pos <= end), gene]
+    if (libraryType == "10X") {
+       bins[[i]] <- paste(chrom, gene_name, strand, sep="_") # include strand in bin name for 10X
+    } else if (libraryType == "SS2") {
+       bins[[i]] <- paste(chrom, gene_name, sep="_") # don't include strand in bin name for SS2 since it is unstranded
+    }
+  }
+  return(unlist(bins))
+}
+
+
 
 ## Determine the strand of the data
 get_strand <- function(strand_col)
@@ -60,7 +91,11 @@ if (nrow(data) > 0) {  # if the input file is empty, don't do any of this.
     data <- data[, c("cell_id", "chr", "pos", "strand", "count","channel")]
 
     ## Create bin from position
-    data <- data[, bin := get_bin(pos, binSize, chr, strand_label)]
+    if (geneVersion == T) {
+       data <- data[, bin := get_bin_gene(pos, binSize, chr, strand_label, libType, gene_bed_path)] 
+    } else {
+       data <- data[, bin := get_bin(pos, binSize, chr, strand_label)]
+    }
 
     ## Output
     output_file_name <- paste(basename, ".count", sep="")
@@ -78,7 +113,11 @@ if (nrow(data) > 0) {  # if the input file is empty, don't do any of this.
     strand_label <- get_strand(data$strand)
 
     ## Create bin from position
-    data <- data[, bin := get_bin(pos, binSize, chr, strand_label)]
+    if (geneVersion == T) {
+       data <- data[, bin := get_bin_gene(pos, binSize, chr, strand_label, libType, gene_bed_path)] 
+    } else {
+       data <- data[, bin := get_bin(pos, binSize, chr, strand_label)]
+    }
 
     ## Output
     output_file_name <- paste(basename, ".count", sep="")
